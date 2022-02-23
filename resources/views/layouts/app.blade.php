b1e8aae92dec2136331716bd4389a2bd072fdf52
Parker <event.roleplay.1@mail.ru>
2022-02-24 01:55:11 +0300

    New pages and category state

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,6 +86,21 @@
                                         <span class="nk-menu-text">Действия игроков</span>
                                     </a>
                                 </li><!-- .nk-menu-item -->
+								<li class="nk-menu-heading">
+                                    <h6 class="overline-title text-primary-alt">Недвижимость</h6>
+                                </li><!-- .nk-menu-heading -->
+								<li class="nk-menu-item">
+                                    <a href="/players" class="nk-menu-link">
+                                        <span class="nk-menu-icon"><em class="icon ni ni-b-uc"></em></span>
+                                        <span class="nk-menu-text">Дома</span>
+                                    </a>
+                                </li><!-- .nk-menu-item -->
+								<li class="nk-menu-item">
+                                    <a href="/players" class="nk-menu-link">
+                                        <span class="nk-menu-icon"><em class="icon ni ni-b-uc"></em></span>
+                                        <span class="nk-menu-text">Бизнесы</span>
+                                    </a>
+                                </li><!-- .nk-menu-item -->
                             </ul><!-- .nk-menu -->
                         </div><!-- .nk-sidebar-menu -->
                     </div><!-- .nk-sidebar-content -->
