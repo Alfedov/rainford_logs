8b49c872b5037e5f654572496efae057465f5ae8
Egor Vozhakov <pirat.vozhakov@yandex.ru>
2022-03-13 00:01:58 +0300

    fix

--- resources/views/pages/players.blade.php
+++ resources/views/pages/players.blade.php
@@ -43,7 +43,7 @@
                                           <a href="#" class="dropdown-toggle btn btn-icon btn-trigger" data-toggle="dropdown"><em class="icon ni ni-more-h"></em></a>
                                           <div class="dropdown-menu dropdown-menu-right">
                                               <ul class="link-list-opt no-bdr">
-                                                  <li><a href="{{ route('player', $users->id) }}"><em class="icon ni ni-eye"></em><span>Посмотреть профиль</span></a></li>
+                                                  <li><a href="{{ route('player', $users->pID) }}"><em class="icon ni ni-eye"></em><span>Посмотреть профиль</span></a></li>
                                               </ul>
                                           </div>
                                       </div>
